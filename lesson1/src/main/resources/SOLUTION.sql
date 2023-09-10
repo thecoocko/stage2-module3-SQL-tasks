fb61200b97d2eadd33c0d8a4d51052c5afad7c4b
kakuracha <krabik4794@gmail.com>
2023-09-10 14:50:07 +0200

    lesson6

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,4 +1,3 @@
-create database university;
 use university;
 create table Student(
                         id bigint Primary key,
@@ -84,3 +83,51 @@ insert into Mark(id,student_id,subject_id,mark) values
                                                     (7,12248,9,4),
                                                     (8,12345,2,9);
 
+alter table Student modify birthday	date not null;
+alter table Mark modify student_id bigint not null;
+alter table Mark modify subject_id bigint not null;
+alter table Mark add CONSTRAINT mark check (mark between 1 and 10);
+alter table Subject add constraint grade check (grade between 1 and 5);
+alter table PaymentType modify name_ varchar(25) unique;
+alter table Payment modify type_id int  NOT NULL;
+alter table Payment modify amount decimal NOT NULL;
+alter table Payment modify payment_date datetime NOT NULL;
+
+select * from student;
+select * from student limit 50;
+select student.name_ from student;
+select distinct(payment.amount) from payment;
+
+select * from payment
+where amount > 500 or amount = 500;
+
+select * from student
+where year(current_date()) - year(student.birthday) > 20;
+
+select * from Student
+where group_ = 10 and year(current_date()) - year(student.birthday) < 20;
+
+select * from Student
+where name_ = 'Mike' or group_  between 4 and 6;
+
+select * from Payment
+where month(current_date()) - month(payment.payment_date) > 8;
+
+select * from Student
+where student.name_ like 'A%';
+
+select * from Student
+where (student.name_ = 'Roxi' and student.group_ = 4) or (student.name_ = 'Tallie' and group_ = 9);
+
+
+select * from payment
+where payment.type_id = 3;
+
+select * from mark
+where mark.subject_id = 1;
+
+select * from (student right join payment on student.id = payment.student_id)
+where payment.type_id = 2;
+
+select * from (student right join mark on student.id = mark.student_id)
+where mark.subject_id = 6;
