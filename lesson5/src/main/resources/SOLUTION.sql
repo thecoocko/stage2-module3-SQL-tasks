b7d469aa47da21aa0d595f79797cd049fece5ab4
kakuracha <krabik4794@gmail.com>
2023-09-15 19:25:08 +0200

    j

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -4,7 +4,7 @@ select * from student where year(current_date()) - year(student.birthday) > 20;
 
 select * from Student where groupnumber = 10 and year(current_date()) - year(student.birthday) < 20;
 
-select * from Student where name = 'Mike' or groupnumber  between 4 and 6;
+select * from Student where name = 'Mike' or groupnumber  between 5 and 7;
 
 select * from Payment where month(current_date()) - month(payment.payment_date) > 8;
 
