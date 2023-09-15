d87424c2eb50c2ca9b61534aac7f413cd339e15b
kakuracha <krabik4794@gmail.com>
2023-09-15 19:43:46 +0200

    message

--- lesson9/src/main/resources/SOLUTION.sql
+++ lesson9/src/main/resources/SOLUTION.sql
@@ -1,19 +1,5 @@
+SELECT st.* FROM Student AS st JOIN Mark AS m ON m.student_id = st.id GROUP BY st.id HAVING AVG(m.mark) > 8;
 
-SELECT st.* FROM Student AS st
-JOIN Mark AS m
-ON m.student_id = st.id
-GROUP BY st.id
-HAVING AVG(m.mark) > 8;
+SELECT st.id, st.name FROM Student AS st JOIN Mark AS m ON m.student_id = st.id GROUP BY st.id HAVING MIN(m.mark) > 7;
 
-SELECT st.id, st.name FROM Student AS st
-JOIN Mark AS m
-ON m.student_id = st.id
-GROUP BY st.id
-HAVING MIN(m.mark) > 7;
-
-SELECT st.id, st.name FROM Student AS st
-LEFT JOIN Payment AS p
-ON p.student_id = st.id
-WHERE YEAR(payment_date) = 2019
-GROUP BY st.id
-HAVING COUNT(p.id) > 2;
+SELECT st.id, st.name FROM Student AS st LEFT JOIN Payment AS p ON p.student_id = st.id WHERE YEAR(payment_date) = 2019 GROUP BY st.id HAVING COUNT(p.id) > 2;
