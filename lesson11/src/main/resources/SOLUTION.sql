fe058f04ea8918d53844ea8be97ebef65129ef0e
kakuracha <krabik4794@gmail.com>
2023-09-15 14:33:48 +0200

    d

--- lesson11/src/main/resources/SOLUTION.sql
+++ lesson11/src/main/resources/SOLUTION.sql
@@ -1,3 +1,8 @@
-UPDATE subject SET grade = 5 WHERE name_ = 'End of Suburbia: Oil Depletion and the Collapse of the American Dream';
-UPDATE payment SET amount = 500, student_id = 2 WHERE payment_date > '2021-01-01' AND type_id = 2;
-UPDATE mark SET mark = 2 WHERE subject_id = 315;
+UPDATE subject
+SET grade = 5
+WHERE name_ = 'End of Suburbia: Oil Depletion and the Collapse of the American Dream';
+UPDATE payment
+SET amount = 500, student_id = 2
+WHERE payment_date > '2021-01-01' AND type_id = 2;
+UPDATE mark
+SET mark = 2 WHERE subject_id = 315;
--- lesson9/src/main/resources/SOLUTION.sql
+++ lesson9/src/main/resources/SOLUTION.sql
@@ -1,5 +1,23 @@
-select * from (student left join mark on student.id = mark.student_id) where mark.mark > (select avg(mark.mark) from mark)+1;
+--## Lesson 9 tasks:
 
-SELECT student.id, student.name FROM student INNER JOIN (SELECT student_id, MIN(mark) AS min_mark FROM mark GROUP BY student_id) as mark ON student.id = mark.student_id WHERE mark.min_mark > 7;
+-- 1) Select all Students from Table Students having average mark more than 8
+SELECT st.* FROM Student AS st
+JOIN Mark AS m
+ON m.student_id = st.id
+GROUP BY st.id
+HAVING AVG(m.mark) > 8;
 
-SELECT student.id, student.name FROM (studentbRIGHT JOIN payment ON student.id = payment.student_id) WHERE payment.payment_date BETWEEN DATE '2019-01-01' AND DATE '2019-12-31' GROUP BY student.id, student.name HAVING COUNT(*) > 1 ORDER BY student.id, student.name;
+-- 2) Select students (id and name) who have minimum mark more than 7
+SELECT st.id, st.name FROM Student AS st
+JOIN Mark AS m
+ON m.student_id = st.id
+GROUP BY st.id
+HAVING MIN(m.mark) > 7;
+
+-- 3) Select all students(id and name) who paid more than 2 times during 2019
+SELECT st.id, st.name FROM Student AS st
+LEFT JOIN Payment AS p
+ON p.student_id = st.id
+WHERE YEAR(payment_date) = 2019
+GROUP BY st.id
+HAVING COUNT(p.id) > 2;
